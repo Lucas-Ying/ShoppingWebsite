8c74a6ea3d914f6b16c7d55fa317ba9d8669e1f0
Jason <difuchen@gmail.com>
2016-05-08 22:17:00 +1200

    view engine

--- app.js
+++ app.js
@@ -9,10 +9,13 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+var port = process.env.PORT ||8080;
+var eng = require('consolidate');
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, '/views'));
+app.engine('html', eng.swig);
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -20,10 +23,17 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/views')));
 
 app.use('/', routes);
 app.use('/users', users);
+
+//render main page
+app.get('/',function(err,res,req,next){
+  res.render('index.html');
+});
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -58,3 +68,7 @@ app.use(function(err, req, res, next) {
 
 
 module.exports = app;
+
+app.listen(port, function () {
+  console.log("ShoppingWebsite app listening on port: "+port+"!");
+});
