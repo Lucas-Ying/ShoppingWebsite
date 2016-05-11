6d5b25d861d41297931e9e2f9e3ff09f4febddaf
James <jimmy2174@gmail.com>
2016-05-11 17:17:32 +1200

    added method to get a single user by email and password

--- app.js
+++ app.js
@@ -275,6 +275,33 @@ app.get('/get_users', function (req,res){
   });
 });
 
+app.get('/get_user', function (req,res){
+  var userEmail = req.body.email;
+  var userPass = req.body.pass;
+console.log(userEmail);
+  var q = "SELECT * FROM users WHERE email=$1;";
+  var query = client.query(q, [userEmail]);
+
+
+  var results =[];
+
+  //error handler for /get_users
+  query.on('error',function(){
+    res.status(500).send('Error, fail to get users: '+userEmail);
+  });
+
+  //stream results back one row at a time
+  query.on('row',function(row){
+    results.push(row);
+  });
+
+  //After all data is returned, close connection and return results
+  query.on('end',function(){
+    res.json(results);
+    console.log("result: "+results);
+  });
+});
+
 //adding users
 app.put('/add_user', function(req, res){
   var userId = req.body.id;
