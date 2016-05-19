c182ec2ee1d4eea70523002e3227b0b13a613657
James Barfoote <james.barfoote@gmail.com>
2016-05-20 10:38:32 +1200

    moved the add user stuff back

--- app.js
+++ app.js
@@ -63,10 +63,28 @@ var q = "SELECT * FROM users WHERE accesstoken=$1 RETURNING accesstoken;";
 
   var results =[];
 
+  var needToAdd = false;
+
   //error handler for /get_users
   query.on('error',function(){
     //res.status(500).send('Error, fail to get users: '+accessToken);
-    console.log("in add user");
+    needToAdd = true;
+  });
+
+  //stream results back one row at a time
+  query.on('row',function(row){
+    results.push(row);
+  });
+
+  //After all data is returned, close connection and return results
+  query.on('end',function(){
+   console.log("Results of get users: " + results);
+  });
+
+ // if (typeof results == 'undefined' || results == null || results.length < 1)
+    if(needToAdd)
+    {
+    	console.log("in add user");
     	//user isnt in the db so we want to add them
     	var q = "insert into users (accesstoken) values ($1)";
   		var query = client.query(q, [accessToken]);
@@ -89,44 +107,7 @@ var q = "SELECT * FROM users WHERE accesstoken=$1 RETURNING accesstoken;";
   			console.log(results)
     		res.json(results);
   		});
-  });
-
-  //stream results back one row at a time
-  query.on('row',function(row){
-    results.push(row);
-  });
-
-  //After all data is returned, close connection and return results
-  query.on('end',function(){
-   console.log("Results of get users: " + results);
-  });
-
- // if (typeof results == 'undefined' || results == null || results.length < 1)
- //    {
- //    	console.log("in add user");
- //    	//user isnt in the db so we want to add them
- //    	var q = "insert into users (accesstoken) values ($1)";
- //  		var query = client.query(q, [accessToken]);
- //  		var results =[];
-
- //  		//error handler 
- //  		query.on('error',function(){
- //   		 res.status(500).send('Error, failed to add new user');
- //  		});
-
- //  		//stream results back one row at a time
- //  		query.on('row',function(row){
- //  		  results.push(row);
- //  		     console.log("Results of add user: " + row)
-
- //  		});
-
- //  		//after all the data is returned close connection and return result
- //  		query.on('end',function(){
- //  			console.log(results)
- //    		res.json(results);
- //  		});
- //    }
+    }
 
 	//if they dont then add them and return to the home page
 	res.redirect('/index.html');
