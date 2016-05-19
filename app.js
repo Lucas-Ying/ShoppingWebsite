531e24541d8fa81df117b5e1757f0dd05559d8cf
James Barfoote <james.barfoote@gmail.com>
2016-05-20 09:55:42 +1200

    changed the if statment that check if the token is valid

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ var q = "SELECT * FROM users WHERE accesstoken=$1;";
 
   //After all data is returned, close connection and return results
   query.on('end',function(){
-    if (typeof results[0] !== 'undefined' && results[0] !== null)
+    if (typeof results !== 'undefined' && results !== null)
     {
     	//user isnt in the db so we want to add them
     	var q = "insert into users (accesstoken) values ($1)";
