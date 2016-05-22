1f782d6a4315bb4c2d3b24f976f70bcfa5d78b70
jason <difuchen@gmail.com>
2016-05-21 22:34:19 -0700

    kart display update

--- public/js/kart.js
+++ public/js/kart.js
@@ -77,28 +77,58 @@ function getKartID(userId){
 
 //display items purchases by user
 function displayItem(kartId){
+    var subtotal = 0;
+    var total = 0;
+    var finalSubtotal = 0;
+    var shipping = 30;
     $.ajax({
         method:'GET',
         url:'/get_purchases',
         success: function(data){
             for(i = 0; i<data.length; i++){
                 if(data[i].cartid == kartId){
-                    var items = data[i].name;
-                    var count = data[i].quantity;
+                    var product = data[i].name;
                     var cost = data[i].price;
+                    var count = data[i].quantity;
+                    subtotal = parseFloat(count)*parseFloat(cost);
+                    finalSubtotal = parseFloat(finalSubtotal)+parseFloat(subtotal);
 
                     //display item
                     var row ="<tr>"
-                    +"<td>"+items+"</td>"
-                    +"<td>"+count+"</td>"
-                    +"<td>"+cost+"</td>" 
+                    +"<td>"+product+"</td>"
+                    +"<td><span class='money'> $"+cost+"</span></td>"
+                    +"<td class='quantity'><input class = 'count' type ='input' value="+count+"></input></td>" 
+                    +"<td><span class='money'> $"+subtotal+"</span></td>"
+                    +"<td>remove button here</td>" 
                     +"</tr>"
 
                     $('#itemTable').append(row);
                 }
             }
+            total = parseFloat(finalSubtotal)+parseFloat(shipping);
+            var endRow = "<tr>"
+                    +"<td></td>"
+                    +"<td></td>"
+                    +"<td></td>" 
+                    +"<td>Subtotal</td>"
+                    +"<td><span class='chout'> $"+finalSubtotal+"</span></td>" 
+                    +"</tr><tr>"
+                    +"<td></td>"
+                    +"<td></td>"
+                    +"<td></td>" 
+                    +"<td>Estimated shipping</td>"
+                    +"<td><span class='chout'> $"+shipping+"</span></td>" 
+                    +"</tr><tr>"
+                    +"<td></td>"
+                    +"<td></td>"
+                    +"<td></td>" 
+                    +"<td>Total</td>"
+                    +"<td><span class='chout'> $"+total+"</span></td>" 
+                    +"</tr>"
+
+             $('#itemTable').append(endRow);
+
         }
     });
 }
-
 
