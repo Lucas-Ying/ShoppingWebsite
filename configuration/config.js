066e049a3829df0936e9e3e6cf6239c681bc75f4
James Barfoote <james.barfoote@gmail.com>
2016-05-23 18:04:26 +1200

    fixed url for callback

--- configuration/config.js
+++ configuration/config.js
@@ -1,9 +1,9 @@
 module.exports={
   "facebook_api_key"      :     "1620018034981698",
   "facebook_api_secret"   :     "3fd05406e3d547dafff4c0bf23bdf33b",
-  "callback_url"          :     "/facebook_callbac",
+  "callback_url"          :     "/auth/facebook/callback",
   "use_database"          :     "false",
-  "host"                  :     "tranquil-journey-51576.herokuapp.com",
+  "host"                  :     "https://www.tranquil-journey-51576.herokuapp.com",
   "username"              :     "root",
   "password"              :     "",
   "database"              :     "DB NAME"
